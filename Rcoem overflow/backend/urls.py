8e545394cc6278100b0744673c0758f908efd663
Bhushan Wanjari <82889656+Bhushan21z@users.noreply.github.com>
2022-05-31 19:07:10 +0530

    Update urls.py

--- Rcoem overflow/backend/urls.py
+++ Rcoem overflow/backend/urls.py
@@ -22,5 +22,6 @@ schema_view = get_schema_view(
 urlpatterns = [
 	path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 	path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
- 
+    path('view_all_questions',views.view_all_questions),
+    path('view_unanswered_questions',views.view_unanswered_questions),
 ]
